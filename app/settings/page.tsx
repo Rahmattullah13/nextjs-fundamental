4ce48a4f28b19b81380914a13f7e5a46129f90c1
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-23 14:22:20 +0700

    learn link and navigation

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import SectionProfile from './sectionProfile';
 
 export default function Settings() {
   return (
     <div>
       <p>This Is A Settings</p>
-      <SectionProfile /> // hanya akan muncul jika dipanggil seperti ini.
+      {/* <SectionProfile /> // hanya akan muncul jika dipanggil seperti ini. */}
     </div>
   );
 }
